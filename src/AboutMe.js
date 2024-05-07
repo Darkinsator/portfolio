314ef1874acb9e382064fefbe40e7aa2f03cf61b
Darkinsator <jasonblankenberg02@gmail.com>
2024-05-07 15:50:24 +0200

    COMMIT

---
+++ src/AboutMe.js
@@ -0,0 +1,10 @@
+const AboutMe = () => {
+    return (
+        <div className="about">
+            <h2>About Me</h2>
+            <p>This is some information about me.</p>
+        </div>
+    );
+}
+
+export default AboutMe;
---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () =>{
+    return(
+        <nav className="navbar">
+            <h1>My Website</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/about">New Blog</a>
+            </div>         
+        </nav>
+    );
+}
+export default Navbar;  
