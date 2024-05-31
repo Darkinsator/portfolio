9cad3db7790b003b2702c9548d60c2e949341679
Darkinsator <jasonblankenberg02@gmail.com>
2024-05-31 14:42:01 +0200

    IMAGES

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './index.css';
 
+
 const AboutMe = () => {
   return (
     <div className="about">
@@ -12,8 +13,21 @@ const AboutMe = () => {
       
       {/* Add more about me content here */}
       <div className="small">
-                    <img src="/image1.png" alt="python"/>
+                    
+                    <img style= {{height: "100px", width: "100px"}} src="/csharp.jpg" alt="C#"/>
+                    <img style= {{height: "100px", width: "130px"}} src="/CSS3.jpg" alt="C#"/>
+                    <img style= {{height: "100px", width: "100px"}} src="/html.jpg" alt="C#"/>
+                    <img style= {{height: "100px", width: "100px"}} src="/JavaScript.jpg" alt="C#"/>                 
+                    <img style= {{height: "100px", width: "100px"}} src="/node.jpg" alt="C#"/>
+                    <img style= {{height: "100px", width: "100px"}} src="/python.jpg" alt="C#"/>
+                    <img style= {{height: "100px", width: "100px"}} src="/react.jpg" alt="C#"/>
+                    <img style= {{height: "100px", width: "100px"}} src="/mysql.jpg" alt="C#"/>
+                    <img style= {{height: "100px", width: "100px"}} src="/Kotlin.jpg" alt="C#"/>
+                    <img style= {{height: "100px", width: "100px"}} src="/java.jpg" alt="C#"/>
+                    <img style= {{height: "100px", width: "60px"}} src="/php.jpg" alt="C#"/>
+
                 </div>
+
     </div>
   );
 };
