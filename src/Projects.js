a3897aaa2406e875dd245c270c26b12ad84ea9ee
Darkinsator <jasonblankenberg02@gmail.com>
2024-06-21 15:56:09 +0200

    ui

--- src/Projects.js
+++ src/Projects.js
@@ -21,9 +21,27 @@ const Projects = () => {
                 </div>
                 <div className="project">
                     <h3>Birdinator Mobile Application</h3>
-                    <p>Created a bird finder application in a team of 3 with kotlin.</p>
+                    <p>Created a bird finder application, in a team of 3 with kotlin.</p>
                     <a href="https://github.com/DeCharter/OPSC_POE_App">View Project</a>
                 </div>
+                <div className="project">
+                    <h3>NGO Web application</h3>
+                    <p>Created a NGO web application where Customers can make donations, in a team of 2.</p>
+                    <a href="https://github.com/Yummy07K/XISDWebApplication">View Project</a>
+                </div>
+                <div className="project">
+                    <h3>Public participation platform web application</h3>
+                    <p>Created public participation web application at a the first inaugural city of cape town hackathon, in a team of 6.</p>
+                    <a href="https://github.com/MogammadShaqeelless16/UVU-AfricaHackathon">View Project</a>
+                </div>
+                <div className="project">
+                    <h3>NGO web application and mobile application</h3>
+                    <p>Created a web application and mobile application demo for the hidden treasures NGO, in a team of 3.</p>
+                    <a href="https://github.com/DeCharter/XISD_MobileApplication">View Project</a>
+                </div>
+                <h3>Want to see more? Head over to my github repository <a href="https://github.com/Darkinsator">here.</a></h3>
+
+
                 {/* Add more projects here */}
             </section>
         </main>
