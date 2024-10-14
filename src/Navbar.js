a8b889797a29010b9eae3d76e209939f389b4ecb
Darkinsator <jasonblankenberg02@gmail.com>
2024-10-14 15:54:18 +0200

    addded navbar goat

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './index.css';
+import './index.css'; // This will include the glitch effect CSS
 
 const Navbar = () => {
   const [showGif, setShowGif] = useState(false);
@@ -15,17 +15,14 @@ const Navbar = () => {
   return (
     <>
       <nav className="navbar">
-        <h1 className="navbarheader">JASON BLANKENBERG</h1>
+        <h1 className="navbarheader" data-text="JASON BLANKENBERG">JASON BLANKENBERG</h1>
         <div className="links">
           <Link className="goldman" to="/">Home</Link>
           <Link className="goldman" to="/about">About Me</Link>
           <Link className="goldman" to="/projects">Projects</Link>
-          <Link className="goldman" to="/contact">Contact Me</Link> {/* New link */}
+          <Link className="goldman" to="/contact">Contact Me</Link> 
           <Link className="goldman" to="/skills">Skills</Link>
         </div>
-        {/* <button className="gif-button" onClick={handleGifClick}> */}
-          {/* <img src={`${process.env.PUBLIC_URL}/tech.png`} alt="Play GIF" /> */}
-        {/* </button> */}
       </nav>
       {showGif && (
         <div className="gif-overlay" onClick={() => setShowGif(false)}>
